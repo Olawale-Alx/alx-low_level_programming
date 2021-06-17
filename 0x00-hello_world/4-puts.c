05f4fa139fc6374a45a17517c2fc28e7d6b34269
Olaleye Olawale <olaleyeolawale2020@outlook.com>
2021-06-17 13:07:29 -0700

    Correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
 int main(void)
 {
-	puts("\"Programming is like building a multi-lingual puzzle ");
+	puts("\"Programming is like building a multilingual puzzle ");
 
 	return (0);
 }
