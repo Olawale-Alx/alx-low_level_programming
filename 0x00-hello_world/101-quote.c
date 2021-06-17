bc7d0859fd728fba503f616b4f1e4864573b0d42
Olaleye Olawale <olaleyeolawale2020@outlook.com>
2021-06-17 14:05:10 -0700

    Adding extra files

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,14 @@
 #include <unistd.h>
 
 /**
- * main - Direct input
+ * main - Entry point
  *
  * Return: 1 "Failure"
  */
 
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
 	return (1);
 }
