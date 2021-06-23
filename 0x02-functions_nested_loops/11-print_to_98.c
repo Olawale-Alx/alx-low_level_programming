d46bb944fa8811157d96dc76ae3ca4f838fe7314
Olaleye Olawale <olaleyeolawale2020@outlook.com>
2021-06-23 13:23:33 -0700

    Correction

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
-#include "holberton.h"
+#include <stdio.h>
+
 
 /**
  * print_to_98 - prints all natural numbers from n to 98
@@ -16,7 +17,7 @@ void print_to_98(int n)
 	{
 		for (; n > 98; n--)
 		{
-			_putchar("%d, ", n);
+			printf("%d, ", n);
 		}
 
 	}
@@ -29,5 +30,5 @@ void print_to_98(int n)
 		}
 	}
 
-	_putchar("%d\n", n);
+	_putchar("%d, ", n);
 }
