96f6d00d7858aa2a965658737e7c840846492ddc
Olaleye Olawale <olaleyeolawale2020@outlook.com>
2021-06-23 13:33:40 -0700

    Correction

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,7 +17,7 @@ void print_to_98(int n)
 	{
 		for (; n > 98; n--)
 		{
-			printf("%d ", n);
+			printf("%d, ", n);
 		}
 
 	}
@@ -30,5 +30,5 @@ void print_to_98(int n)
 		}
 	}
 
-	printf("%d,\n", n);
+	printf("%d\n", n);
 }
