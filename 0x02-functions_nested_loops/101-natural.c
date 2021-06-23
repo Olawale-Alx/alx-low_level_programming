be6638675237755dd8e435337d7ad9fc410369d8
Olaleye Olawale <olaleyeolawale2020@outlook.com>
2021-06-23 13:57:16 -0700

    Adding

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints natural numbers below 1024 that are multipliers of 3 or 5
+ *
+ *Return: 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int j, k;
+
+	for (j = 1; j < 1024; j++)
+	{
+		if ((a % 3) == 0 || (a % 5) == 0)
+		{
+			k = k + j;
+		}
+
+	}
+
+	printf("%d\n", k);
+	return (0);
+
+}
