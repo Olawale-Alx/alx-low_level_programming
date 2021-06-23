e3c0f4e12190c3283fe0dce5c0b62555eb95e665
Olaleye Olawale <olaleyeolawale2020@outlook.com>
2021-06-23 08:35:53 -0700

    Adding first test

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ * main prints the word 'holberton'
+ * Return: 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int i;
+	char p[] = "Holberton";
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(p[i]);
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
