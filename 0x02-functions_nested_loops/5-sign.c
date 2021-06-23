98f62f5cc1094cf166b2e45e130050e3a5e7d9fc
Olaleye Olawale <olaleyeolawale2020@outlook.com>
2021-06-23 12:39:56 -0700

    Correction

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,30 +1,32 @@
 #include "holberton.h"
 
 /**
- * print_sign
+ * print_sign - Returns sign
  *
- * Return
+ * @n: The character in ASCII code
+ *
+ * Return: 1 if n is greater than zero, 0 if n is zero, -1 if less than zero
  *
  */
 
-int print_sign(int p)
+int print_sign(int n)
 {
 	if (n > 0)
 	{
 		_putchar(43);
-		return (1);
+	return (1);
 	}
 
 	else if (n < 0)
 	{
 		_putchar(45);
-		return (-1);
+	return (-1);
 	}
 
 	else
 	{
 		_putchar(48);
-		return (0);
+	return (0);
 	}
 
 	_putchar('\n');
