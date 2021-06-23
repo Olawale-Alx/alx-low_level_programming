e572d39909c2b8a82117b3717443ff6e15426909
Olaleye Olawale <olaleyeolawale2020@outlook.com>
2021-06-23 12:04:28 -0700

    Adding and correcting

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * print_alphabetx10 - Prints the alphabet in lowercase x10
+ * print_alphabet_x10 - Prints the alphabet in lowercase x10
  *
  * Return: 0 (success)
  *
@@ -11,7 +11,7 @@ void print_alphabet_x10(void)
 {
 	int c, t;
 
-	for (c = 1; c <=9; c++)
+	for (c = 0; c < 10; c++)
 	{
 		for (t = 97; t <= 122; t++)
 		{
