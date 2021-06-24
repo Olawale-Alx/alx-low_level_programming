30b4fdfce2ad02b79f23c7dea8a10a612596830e
Olaleye Olawale <olaleyeolawale2020@outlook.com>
2021-06-24 13:35:43 -0700

    Adding new file(s)

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - Prints number 0 to 14 ten times
+ *
+ * Return: no return
+ *
+ */
+
+void more_numbers(void)
+{
+	int q;
+	char p;
+
+	for (q = 1; q <= 10; q++)
+	{
+		for (p = 0; p <= 14; p++)
+		{
+			if (p > 9)
+			{
+				_putchar((p / 10) + '0');
+				_putchar((p % 10) + '0');
+			}
+		}
+
+		_putchar('\n');
+
+	}
+}
