4f4e06f6a978ca9ce5d460bce77699f1de43e739
Olaleye Olawale <olaleyeolawale2020@outlook.com>
2021-06-24 13:09:33 -0700

    Adding new file(s)

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - Prints all numbers from
+ * 0 to 9 except 2 and 4
+ *
+ * Return: no return
+ *
+ */
+
+void print_most_numbers(void)
+{
+	char n;
+
+	for (let n = '0'; n <= '9'; n++)
+	{
+		if (n != '2' && n != '4')
+		{
+			_putchar(n);
+		}
+
+		_putchar('\n');
+	}
+}
