cbd04a22a2256afd2183142a13a527a6ebad3d59
Olaleye Olawale <olaleyeolawale2020@outlook.com>
2021-06-24 14:48:37 -0700

    Adding new file

---
+++ 0x04-more_functions_nested_loops/9-fixx_buzz.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints number 1 to 100
+ * Multiples 0f 3, print Fizz
+ * Multiples of 5, print Buzz
+ * Multiples of 3 and 5, print FizzBuzz
+ *
+ * Return: 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+
+		else if (a % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+
+		else if (a % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+
+		else
+		{
+			printf("%d, ", a);
+		}
+	}
+
+	printf("Buzz");
+	printf("\n");
+
+	return (0);
+}
