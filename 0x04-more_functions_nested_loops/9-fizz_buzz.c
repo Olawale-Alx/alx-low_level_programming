51516f97dee27bd2ad7bd1067b44110dfaef785d
Olaleye Olawale <olaleyeolawale2020@outlook.com>
2021-06-24 14:53:06 -0700

    Correction

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,7 +15,7 @@ int main(void)
 {
 	int a;
 
-	for (a = 1; a <= 100; a++)
+	for (a = 1; a < 100; a++)
 	{
 		if (a % 3 == 0)
 		{
@@ -34,7 +34,7 @@ int main(void)
 
 		else
 		{
-			printf("%d, ", a);
+			printf("%d ", a);
 		}
 	}
 
