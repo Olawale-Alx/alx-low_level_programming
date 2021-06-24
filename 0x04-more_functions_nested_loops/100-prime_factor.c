e190cf58e1a7344484d6eb3d2a30a436e6c3edc3
Olaleye Olawale <olaleyeolawale2020@outlook.com>
2021-06-24 15:32:57 -0700

    Adding new file(s)

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * @void - Function has no argument
+ *
+ * Return: 0 (success)
+ *
+ */
+
+
+int main(void)
+{
+	long m, num = 612852475143;
+
+	for (m = 1 m <= num, m++)
+	{
+		if (num % m == 0)
+		{
+			num = num / m;
+			m--;
+		}
+	}
+
+	printf("%lu\n", m);
+
+	return (0);
+}
