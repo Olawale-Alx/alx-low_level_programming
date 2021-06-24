819033385fa0aa33d107106eb1f3666a2df15f86
Olaleye Olawale <olaleyeolawale2020@outlook.com>
2021-06-24 10:02:56 -0700

    Adding new file(s)

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - Prints numbers from 0 to 9
+ *
+ * Return: No return
+ *
+ */
+
+void print_numbers(void)
+{
+	int j;
+
+	for (j >= '0'; j <= '9'; j++)
+	{
+		_putchar(j);
+	}
+
+	_putchar('\n');
+}
