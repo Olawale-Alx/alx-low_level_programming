5f64b926ca35a108fcc6f9c8f42da2386bd70c00
Olaleye Olawale <olaleyeolawale2020@outlook.com>
2021-06-24 09:39:06 -0700

    Adding new file(s)

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * mul - This function multiplies two integers
+ *
+ * @a: First integer
+ *
+ * @b: Second Integer
+ *
+ * Return: Multiplication of both integers
+ * mul(3, 4) == 12
+ *
+ */
+
+int mul(int a, int b)
+{
+	int t;
+
+	t = a * b;
+
+return (t);
+}
