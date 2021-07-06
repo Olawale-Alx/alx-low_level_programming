fa3e423af882424af3f31ed0ff851f1e88ee410a
Olaleye Olawale <olaleyeolawale2020@outlook.com>
2021-07-06 12:35:08 -0700

    Adding extra file(s)

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * set_string - sets the value of a pointer to a character
+ *
+ * @s: Character value
+ * @to: pointer
+ *
+ * Return: null
+ *
+ */
+
+void set_string(char **s, char **to)
+{
+	*s = to;
+}
