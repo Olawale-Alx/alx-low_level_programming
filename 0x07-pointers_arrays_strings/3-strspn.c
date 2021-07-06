aa142210801b880824ca588461f63aa6a70e1c81
Olaleye Olawale <olaleyeolawale2020@outlook.com>
2021-07-06 13:03:36 -0700

    Correction

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,4 +1,4 @@
-0;10;1c#include "holberton.h"
+#include "holberton.h"
 
 /**
  * _strspn - Gets the length of a prefix substring
@@ -31,7 +31,6 @@ unsigned int _strspn(char *s, char *accept)
 		{
 			return (len);
 		}
-		
 	}
 
 	return (len);
