26262eeca1371a33fe3615d18a9e680cc89d4321
Olaleye Olawale <olaleyeolawale2020@outlook.com>
2021-07-13 14:28:01 -0700

    Adding files

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * main - this program prints all arguements it receives
+ *
+ * @argc: arguement count
+ * @argv: array of pointers to arguement
+ *
+ * Return: 0 if successful and 1 if not
+ *
+ */
+
+int main(int argc, int *argv[])
+{
+	while (argc--)
+	{
+		printf("%s\n", *argv[]);
+	}
+
+	return (0);
+}
