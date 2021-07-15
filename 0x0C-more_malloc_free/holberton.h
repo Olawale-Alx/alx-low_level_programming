bb920a9e3b85ccbf9ed73653ab7a43231412fdda
Olaleye Olawale <olaleyeolawale2020@outlook.com>
2021-07-15 10:58:08 -0700

    Adding file(s)

---
+++ 0x0C-more_malloc_free/holberton.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int *array_range(int min, int max);
+void *_calloc(unsigned int nmemb, unsigned int size);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *malloc_checked(unsigned int b);
+int _putchar(char c);
